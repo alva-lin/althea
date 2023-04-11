b6bf8912e7947fae1a674f933ca931bea33706ba
Alva <asjun2lin@outlook.com>
2023-04-11 18:06:51 +0800

    feat: 修改名字

--- Infrastructure/Althea.Infrastructure.AspNetCore/Extensions/MiddlewareExtension.cs
+++ Infrastructure/Althea.Infrastructure.AspNetCore/Extensions/MiddlewareExtension.cs
@@ -1,10 +1,9 @@
 ﻿using Althea.Infrastructure.AspNetCore.Wrapper;
-
 using Microsoft.AspNetCore.Builder;
 
 namespace Althea.Infrastructure.AspNetCore.Extensions;
 
-public static class MiddlewaveExtension
+public static class MiddlewareExtension
 {
     public static IApplicationBuilder UseBasicException(this IApplicationBuilder host)
     {
